13fb91897a0c3f1f976b27a970cb70745bf83474
Shannon Nabors <shannon.nabors@gmail.com>
2018-11-12 19:39:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-var katzDeli = [];
+/*function takeANumber(line, newName) {
+  line.push(newName);
+  return("Welcome, " + newName + ". You are number " + line.length + " in line.");
+}*/
 
-function takeANumber(katzDeli, newName) {
-  katzDeli.push(newName);
-  return("Welcome, " + newName + ". You are number " + katzDeli.length + " in line.");
+var line = [1, 2]
+
+function takeANumber(line) {
+  line.push(line[line.length - 1] + 1);
+  return(line.length);
 }
 
-function nowServing(katzDeli) {
-  if (katzDeli.length > 0) {
-    var currentCustomer = katzDeli[0];
-    katzDeli.shift();
+console.log(takeANumber(line));
+
+function nowServing(line) {
+  if (line.length > 0) {
+    var currentCustomer = line[0];
+    line.shift();
     return("Currently serving " + currentCustomer + ".");
   } else {
     return("There is nobody waiting to be served!");
@@ -17,23 +24,28 @@ function nowServing(katzDeli) {
 
 function currentLine(line) {
   if (line.length > 0) {
-    var deliLine = [];
+    var lineString = [];
       for (var i = 0; i < line.length; i++) {
-        deliLine.push(" " + (i + 1) + ". " + line[i]);
+        lineString.push(" " + (i + 1) + ". " + line[i]);
       }
-    return("The line is currently:" + deliLine.toString());
+    return("The line is currently:" + lineString.toString());
   } else {
     return("The line is currently empty.");
   }
 }
 
+/*let katzDeli = ["Greg"];
 
-
-takeANumber(katzDeli, "Rex");
-takeANumber(katzDeli, "Jason");
-takeANumber(katzDeli, "Bart");
-console.log(katzDeli);
-nowServing(katzDeli);
-console.log(katzDeli);
-nowServing(katzDeli);
-console.log(katzDeli);
+console.log(takeANumber(katzDeli, "Rex"));
+console.log(takeANumber(katzDeli, "Jason"));
+console.log(takeANumber(katzDeli, "Bart"));
+console.log(currentLine(katzDeli));
+console.log(nowServing(katzDeli));
+console.log(currentLine(katzDeli));
+console.log(nowServing(katzDeli));
+console.log(currentLine(katzDeli));
+console.log(nowServing(katzDeli));
+console.log(currentLine(katzDeli));
+console.log(nowServing(katzDeli));
+console.log(currentLine(katzDeli));
+console.log(nowServing(katzDeli));*/
